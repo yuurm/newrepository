12d503bb360790eab18bda408101d5c0f210b161
Yuri Urmatsky <yuriurmatsky@gmail.com>
2022-05-11 20:31:53 +0300

    new changes in App

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App{
 
      public static void main (String[] args){
-        System.out.println ("Hello Top Academy");
+        System.out.println ("Hello Top Academy Repository");
    }
 
 }
